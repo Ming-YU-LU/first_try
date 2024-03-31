f1cb06ca8ac514dd7e1ac5654530dee3a84f9a48
Yu-lu-ming <ggso@outlook.com>
2024-03-31 23:19:22 +0800

    feat: firstCommit

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true
+    },
+    // 指定如何解析语法
+    parser: 'vue-eslint-parser',
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                ".eslintrc.{js,cjs}"
+            ],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+        jsxPragma: 'React',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: [
+        "@typescript-eslint",
+        "vue"
+    ],
+    rules: {
+        'no-var': 'error',
+        'no-multiple-empty-lines': [1, { max: 1 }],
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-unexpected-multiline': 'error',
+
+        // typescript-eslint
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/prefer-ts-expect-error': 'off', // 是否可以使用@ts-ignore
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off', // 确保没有为null或undefined的断言
+        '@typescript-eslint/no-namespace': 'off', // 禁止自定义的typescript模块和命名空间
+        '@typescript-eslint/no-semi': 'off', // 禁止使用分号
+
+        // vue plugin eslint
+        'vue/multi-word-component-names': 'off', // 组件名使用-连接多个单词
+        'vue/script-setup-uses-vars': 'error', // 防止script setup中的变量被<template>标记为未使用
+        'vue/no-mutating-props': 'off',
+        'vue/attribute-hyphenation': 'off' // vue属性命名驼峰
+    }
+}
---
+++ .stylelintrc.cjs
@@ -0,0 +1,52 @@
+export default {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-html/vue',
+        'stylelint-config-standard-scss',
+        'stylelint-recommended-vue/scss',
+        'stylelint-config-recess-order',
+        'stylelint-prettier/recommended'
+    ],
+    plugins: ['stylelint-prettier'],
+    override: [
+        {
+            files: [
+                '**/*.(scss|css|vue|html)'
+            ],
+            customSyntax: 'postcss-scss',
+        },
+        {
+            files: [
+                '**/*.(vue|html)'
+            ],
+            customSyntax: 'postcss-html'
+        }
+    ],
+    ignoreFiles: [
+        '**/*.js',
+        '**/*.jsx',
+        '**/*.tsx',
+        '**/*.ts',
+        '**/*.md',
+        '**/*.json',
+        '**/*.yaml'
+    ],
+    rules: {
+        'prettier/prettier': true,
+        'value-keyword-case': null,
+        'no-descending-specificity': null,
+        'function-url-quotes': 'always',
+        'no-empty-source': null,
+        'selector-class-pattern': null,
+        'property-no-unknown': null,
+        'block-opening-brace-space-before': 'always',
+        'value-no-vendor-prefix': null,
+        'property-no-vendor-prefix': null,
+        'select-pseudo-class-no-unknown': [
+            true,
+            {
+                ignorePseudoClasses: ['global', 'v-deep', 'deep'] // 忽略的属性 修改element 默认样式用的上
+            }
+        ]
+    }
+}
